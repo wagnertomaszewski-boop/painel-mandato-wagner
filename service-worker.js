80f2f471f606357dedd922f312ec55e186f69ae2
wagnertomaszewski-boop <wagner.tomaszewski@gmail.com>
2026-09-08 13:39:17 -0300

    Create service-worker.js

---
+++ service-worker.js
@@ -0,0 +1,13 @@
+const CACHE_NAME = "painel-mandato-v1";
+
+self.addEventListener("install", () => {
+  self.skipWaiting();
+});
+
+self.addEventListener("activate", event => {
+  event.waitUntil(self.clients.claim());
+});
+
+self.addEventListener("fetch", event => {
+  event.respondWith(fetch(event.request));
+});
